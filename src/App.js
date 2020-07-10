3b5fb0f260d642374305f60a846fe861110ca88d
Paul T.Matik <t.matik1@gmail.com>
2020-07-10 23:50:37 +0700

    Preapare loading sections

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
 import Topbar from "./components/Topbar";
 
+import "./styles/Page.css";
+
 function App() {
   return (
     <>
       <Topbar companyName="ООО «Ромашка обыкновенная»" tin="1234567890" />
+      <main className="page__main">
+        <h1 className="page__h1">Загрузите документацию</h1>
+        <section className="page__load_section">
+          <h2 className="page__h2">
+            Загрузите копию паспорта директора и учредителей компании: основной
+            разворот и регистрация.
+          </h2>
+        </section>
+
+        <section className="page__load_section">
+          <h2 className="page__h2">
+            Загрузите банковские выписки за последние 24 месяца в формате .txt
+            (1C) по следующим счетам:
+          </h2>
+        </section>
+      </main>
     </>
   );
 }
